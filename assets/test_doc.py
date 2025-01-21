b8e78be0803b9fa4cfa9ac2b815171a694275330
Simran <dcube0710@gmail.com>
2025-01-22 00:06:14 +0530

    first

--- assets/test_doc.py
+++ assets/test_doc.py
@@ -2,8 +2,215 @@ api_docs = [
     {
         "name": "Funding Milestones",
         "endpoint": "https://api.crustdata.com/data_lab/funding_milestone_timeseries/",
-        "description": "Use this request to get a time-series of funding milestones with  company_id equal to any one of [637158, 674265, 674657]",
-        "curl": 'curl --request POST --url https://api.crustdata.com/data_lab/funding_milestone_timeseries/ --header \'Accept: application/json, text/plain, */*\' --header \'Accept-Language: en-US,en;q=0.9\' --header \'Authorization: Token $auth_token\' --header \'Content-Type: application/json\' --header \'Origin: https://crustdata.com\' --header \'Referer: https://crustdata.com/\' --data \'{"filters":{"op":"or","conditions":[{"column":"company_id","type":"in","value":[637158,674265,674657]}]},"offset":0,"count":1000,"sorts":[]}\'',
+        "description": "Use this request to get a time-series of funding milestones with company_id equal to any one of [637158, 674265, 674657]",
+        "curl": '''curl --request POST --url https://api.crustdata.com/data_lab/funding_milestone_timeseries/ --header 'Accept: application/json, text/plain, */*' --header 'Accept-Language: en-US,en;q=0.9' --header 'Authorization: Token $auth_token' --header 'Content-Type': 'application/json' --header 'Origin': 'https://crustdata.com' --header 'Referer': 'https://crustdata.com/' --data '{"filters":{"op":"or","conditions":[{"column":"company_id","type":"in","value":[637158,674265,674657]}]},"offset":0,"count":1000,"sorts":[]}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $auth_token',
+    'Content-Type': 'application/json',
+    'Origin': 'https://crustdata.com',
+    'Referer': 'https://crustdata.com',
+}
+json_data = {
+    'filters': {
+        'op': 'or',
+        'conditions': [
+            {'column': 'company_id', 'type': 'in', 'value': [637158, 674265, 674657]},
+        ],
+    },
+    'offset': 0,
+    'count': 1000,
+    'sorts': [],
+}
+response = requests.post('https://api.crustdata.com/data_lab/funding_milestone_timeseries/', headers=headers, json=json_data)
+print(response.text)''',
         "response": "https://jsonhero.io/j/XDfprlYDbOvf",
     },
+    {
+        "name": "Decision Makers/People Info",
+        "endpoint": "https://api.crustdata.com/data_lab/decision_makers/",
+        "description": "This request retrieves decision maker data for a given company. It allows filtering based on titles, LinkedIn profile URLs, and more.",
+        "curl": '''curl --request POST --url https://api.crustdata.com/data_lab/decision_makers/ --header 'Accept: application/json, text/plain, */*' --header 'Accept-Language: en-US,en;q=0.9' --header 'Authorization: Token $auth_token' --header 'Content-Type': 'application/json' --header 'Origin': 'http://localhost:3000' --header 'Referer': 'http://localhost:3000/' --data '{"filters":{"op": "and", "conditions": [{"column": "company_id", "type": "in", "value": [632328]}]},"offset":0,"count":100,"sorts":[]}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $auth_token',
+    'Content-Type': 'application/json',
+    'Origin': 'http://localhost:3000',
+    'Referer': 'http://localhost:3000',
+}
+json_data = {
+    'filters': {
+        'op': 'and',
+        'conditions': [
+            {'column': 'company_id', 'type': 'in', 'value': [632328]},
+        ],
+    },
+    'offset': 0,
+    'count': 100,
+    'sorts': [],
+}
+response = requests.post('https://api.crustdata.com/data_lab/decision_makers/', headers=headers, json=json_data)
+print(response.text)''',
+        "response": "https://jsonhero.io/j/QSAlhbuflhie",
+    },
+    {
+        "name": "LinkedIn Employee Headcount and LinkedIn Follower Count",
+        "endpoint": "https://api.crustdata.com/data_lab/headcount_timeseries/",
+        "description": "Use this request to get weekly and monthly time series of employee headcount as a JSON blob.",
+        "curl": '''curl 'https://api.crustdata.com/data_lab/headcount_timeseries/' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.9' -H 'Authorization: Token $auth_token' -H 'Content-Type': 'application/json' -H 'Origin': 'https://crustdata.com' -H 'Referer': 'https://crustdata.com' --data-raw '{"filters":{"op":"or","conditions":[{"column":"company_id","type":"=","value":634995},{"column":"company_id","type":"=","value":680992},{"column":"company_id","type":"=","value":673947},{"column":"company_id","type":"=","value":631811}]},"offset":0,"count":100,"sorts":[]}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $auth_token',
+    'Content-Type': 'application/json',
+    'Origin': 'https://crustdata.com',
+    'Referer': 'https://crustdata.com',
+}
+json_data = {
+    'filters': {
+        'op': 'or',
+        'conditions': [
+            {'column': 'company_id', 'type': '=', 'value': 634995},
+            {'column': 'company_id', 'type': '=', 'value': 680992},
+            {'column': 'company_id', 'type': '=', 'value': 673947},
+            {'column': 'company_id', 'type': '=', 'value': 631811},
+        ],
+    },
+    'offset': 0,
+    'count': 100,
+    'sorts': [],
+}
+response = requests.post('https://api.crustdata.com/data_lab/headcount_timeseries/', headers=headers, json=json_data)
+print(response.text)''',
+        "response": "https://jsonhero.io/j/bd2OKMSu8ZQ0/editor"
+    },
+    {
+        "name": "Glassdoor Profile Metrics",
+        "endpoint": "https://api.crustdata.com/data_lab/glassdoor_profile_metric/Table/",
+        "description": "Use this request to get the rating of a company on Glassdoor.",
+        "curl": '''curl --request POST --url https://api.crustdata.com/data_lab/glassdoor_profile_metric/Table/ --header 'Accept: application/json, text/plain, */*' --header 'Accept-Language': 'en-US,en;q=0.9' --header 'Authorization: Token $token' --header 'Content-Type': 'application/json' --header 'Origin': 'https://crustdata.com' --data '{"tickers":[],"dataset":{"name":"glassdoor_profile_metric","id":"glassdoorprofilemetric"},"filters":{"op":"and","conditions":[{"column":"company_id","type":"in","value":[680992,673947,631280,636304,631811],"allow_null":false}]},"groups":[],"aggregations":[],"functions":[],"offset":0,"count":100,"sorts":[]}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $token',
+    'Content-Type': 'application/json',
+    'Origin': 'https://crustdata.com',
+}
+json_data = {
+    'tickers': [],
+    'dataset': {'name': 'glassdoor_profile_metric', 'id': 'glassdoorprofilemetric'},
+    'filters': {
+        'op': 'and',
+        'conditions': [
+            {'column': 'company_id', 'type': 'in', 'value': [680992, 673947, 631280, 636304, 631811], 'allow_null': False},
+        ],
+    },
+    'offset': 0,
+    'count': 100,
+    'sorts': [],
+}
+response = requests.post('https://api.crustdata.com/data_lab/glassdoor_profile_metric/Table/', headers=headers, json=json_data)
+print(response.text)''',
+        "response": "https://jsonhero.io/j/SdGsOnEIJ33x/editor"
+    },
+    {
+        "name": "G2 Profile Metrics",
+        "endpoint": "https://api.crustdata.com/data_lab/g2_profile_metric/",
+        "description": "Get company G2 profile data for reviews and scores on G2.",
+        "curl": '''curl --request POST --url https://api.crustdata.com/data_lab/g2_profile_metric/ --header 'Accept: application/json, text/plain, */*' --header 'Accept-Language': 'en-US,en;q=0.9' --header 'Authorization': 'Token $auth_token' --header 'Content-Type': 'application/json' --header 'Origin': 'https://crustdata.com' --data '{"filters":{"op":"or","conditions":[{"column":"company_id","type":"in","value":[637158,674265,674657]}]},"offset":0,"count":1000,"sorts":[]}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $auth_token',
+    'Content-Type': 'application/json',
+    'Origin': 'https://crustdata.com',
+    'Referer': 'https://crustdata.com',
+}
+json_data = {
+    'filters': {
+        'op': 'or',
+        'conditions': [
+            {'column': 'company_id', 'type': 'in', 'value': [637158, 674265, 674657]},
+        ],
+    },
+    'offset': 0,
+    'count': 1000,
+    'sorts': [],
+}
+response = requests.post('https://api.crustdata.com/data_lab/g2_profile_metric/', headers=headers, json=json_data)
+print(response.text)''',
+        "response": "https://jsonhero.io/j/aGHvIu7d9k6N",
+    },
+    {
+        "name": "Web Traffic",
+        "endpoint": "https://api.crustdata.com/data_lab/web_traffic/",
+        "description": "Use this request to get the weekly and monthly time series of web traffic for a company. The data is retrieved in a JSON blob.",
+        "curl": '''curl --request POST --url https://api.crustdata.com/data_lab/web_traffic/ --header 'Accept: application/json, text/plain, */*' --header 'Accept-Language: en-US,en;q=0.9' --header 'Authorization: Token $auth_token' --header 'Content-Type': 'application/json' --header 'Origin': 'https://crustdata.com' --header 'Referer': 'https://crustdata.com' --data '{"filters":{"op":"or","conditions":[{"column":"company_id","type":"in","value":[637158,674265,674657]}]},"offset":0,"count":1000,"sorts":[]}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $auth_token',
+    'Content-Type': 'application/json',
+    'Origin': 'https://crustdata.com',
+    'Referer': 'https://crustdata.com',
+}
+json_data = {
+    'filters': {
+        'op': 'or',
+        'conditions': [
+            {'column': 'company_id', 'type': 'in', 'value': [637158, 674265, 674657]},
+        ],
+    },
+    'offset': 0,
+    'count': 1000,
+    'sorts': [],
+}
+response = requests.post('https://api.crustdata.com/data_lab/web_traffic/', headers=headers, json=json_data)
+print(response.text)''',
+        "response": "https://jsonhero.io/j/aGHvIu7d9k6N",
+    },
+    {
+        "name": "Investor Portfolio",
+        "endpoint": "https://api.crustdata.com/data_lab/investor_portfolio/",
+        "description": "Use this request to get the portfolio details of an investor, either by UUID or name.",
+        "curl": '''curl --request GET --url https://api.crustdata.com/data_lab/investor_portfolio/ --header 'Accept: application/json, text/plain, */*' --header 'Accept-Language: en-US,en;q=0.9' --header 'Authorization: Token $auth_token' --data '{"investor_uuid": "UUID_placeholder"}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $auth_token',
+}
+params = {
+    'investor_uuid': 'UUID_placeholder',  # Replace with actual UUID or name
+}
+response = requests.get('https://api.crustdata.com/data_lab/investor_portfolio/', headers=headers, params=params)
+print(response.text)''',
+        "response": "https://jsonhero.io/j/bZ2YSQljvYwB",
+    },
+    {
+        "name": "Investor Portfolio by Name",
+        "endpoint": "https://api.crustdata.com/data_lab/investor_portfolio_by_name/",
+        "description": "This request fetches investor portfolio data by the investor's name.",
+        "curl": '''curl --request GET --url https://api.crustdata.com/data_lab/investor_portfolio_by_name/ --header 'Accept: application/json, text/plain, */*' --header 'Accept-Language: en-US,en;q=0.9' --header 'Authorization: Token $auth_token' --data '{"investor_name": "Investor Name"}' ''',
+        "python": '''import requests
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Token $auth_token',
+}
+params = {
+    'investor_name': 'Investor Name',  # Replace with actual investor name
+}
+response = requests.get('https://api.crustdata.com/data_lab/investor_portfolio_by_name/', headers=headers, params=params)
+print(response.text)''',
+        "response": "https://jsonhero.io/j/5yIb8n1xYv3D",
+    }
 ]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, jsonify
 import os
 import logging
 from utility.api_doc_processor import ApiDocProcessor
@@ -10,11 +11,16 @@ from langchain import hub
 from utility.state import State
 from langgraph.graph import START, StateGraph
 
+# Initialize logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
+# Initialize Flask app
+app = Flask(__name__)
+
+# Configuration values
 embeddings_model = str(config.EMBEDDINGS_MODEL_NAME)
 pinecone_api_key = str(config.PINECONE_ACCESS_TOKEN)
 index_name = str(config.INDEX_NAME)
@@ -23,61 +29,61 @@ groq_model_name = str(config.GROQ_MODEL_NAME)
 prompt_template = str(config.PROMPT_TEMPLATE_NAME)
 langsmith_api_key = str(config.LANGSMITH_API_KEY)
 
+# Set environment variables
 os.environ["PINECONE_API_KEY"] = pinecone_api_key
 os.environ["GROQ_API_KEY"] = groq_api_key
 os.environ["LANGSMITH_API_KEY"] = langsmith_api_key
 
+# Initialize components
 embeddings = Embeddings().get_huggingface_embeddings(model_name=embeddings_model)
-
 vector_store = VectorStore(embeddings=embeddings).get_pinecone_vector_store(
     pinecone_api_key=pinecone_api_key, index_name=index_name
 )
-
 llm = LLM().get_groq_llm(model_name=groq_model_name)
-
 prompt = hub.pull(prompt_template)
 
-
+# Function to retrieve documents
 def retrieve(state: State):
     retrieved_docs = vector_store.similarity_search(state["question"])
     return {"context": retrieved_docs}
 
-
+# Function to generate an answer
 def generate(state: State):
     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
     messages = prompt.invoke({"question": state["question"], "context": docs_content})
     response = llm.invoke(messages)
     return {"answer": response.content}
 
+# Process API docs and initialize vector store
+docs = ApiDocProcessor(api_docs=api_docs).process_api_docs()
+logging.info(f"Seeding {len(docs)} documents to the vector store")
+documents_ids = vector_store.add_documents(documents=docs)
+logging.info(f"Documents IDs {documents_ids[:3]}")
 
-def main():
-    docs = ApiDocProcessor(api_docs=api_docs).process_api_docs()
-    logging.info(f"Seeding {len(docs)} documents to the vector store")
-    documents_ids = vector_store.add_documents(documents=docs)
-    logging.info(f"Documents IDs {documents_ids[:3]}")
+# Build state graph
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
 
-    graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-    graph_builder.add_edge(START, "retrieve")
-    graph = graph_builder.compile()
+# Define the chatbot HTML route
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    print("Ask a question (type 'exit' to quit):")
-    while True:
-        try:
-            question = input(">>> ")
-            if question.strip().lower() == "exit":
-                print("Exiting the program. Goodbye!")
-                break
-            
-            response = graph.invoke({"question": question})
-            print("\nAnswer:")
-            print(response["answer"])
-        except KeyboardInterrupt:
-            print("\nExiting the program. Goodbye!")
-            break
-        except Exception as e:
-            logging.error(f"An error occurred: {e}")
-            print("An error occurred. Please try again.")
+# Define an API endpoint for chatbot interaction
+@app.route('/chat', methods=['POST'])
+def chat():
+    try:
+        user_input = request.json.get("message")
+        if not user_input:
+            return jsonify({"error": "Invalid input"}), 400
 
+        # Use the state graph to get a response
+        response = graph.invoke({"question": user_input})
+        return jsonify({"response": response["answer"]})
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return jsonify({"error": "Something went wrong. Please try again."}), 500
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
